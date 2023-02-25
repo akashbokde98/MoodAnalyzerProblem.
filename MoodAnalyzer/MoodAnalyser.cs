47105a310f719482c2b6b9564ff127839a4759f5
akashbokde98 <akashbokde98@gmail.com>
2023-02-26 01:26:56 +0530

    Added: Analyzing Mood

---
+++ MoodAnalyzer/MoodAnalyser.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+
+namespace MoodAnalyzer
+{
+    public class MoodAnalyser
+    {
+        public string message;
+        public MoodAnalyser(string message) 
+        {
+            this.message = message;
+        }
+        public string AnalyseMood()
+        {
+            if(message.Contains("Sad"))
+            {
+                return "Sad";
+            }
+            else
+            {
+                return "Happy";
+            }
+        }
+    }
+}
