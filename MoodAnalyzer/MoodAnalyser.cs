80691b0229f7e69379a54c2dfaa9c0b2c2ad5af0
akashbokde98 <akashbokde98@gmail.com>
2023-02-27 01:20:54 +0530

    Added:Handle Exception

---
+++ MoodAnalyzer/MoodAnalyser.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MoodAnalyzer
+{
+    public class MoodAnalyser
+    {
+        public string message;
+        public string mood = null;
+        public MoodAnalyser(string message)
+        {
+            this.message = message;
+        }
+        public string AnalyseMood()
+        {
+            try
+            {
+                if (message.Contains(mood))
+                {
+                    return "Sad";
+                }
+                throw new Exception();
+            }
+            catch(Exception)
+            {
+                Console.WriteLine("Entered Invalid or Null Mood Name");
+            }
+            return "Happy";
+        }
+    }
+}
---
+++ UnitTest/UnitTest1.cs
@@ -0,0 +1,40 @@
+using NUnit.Framework;
+using MoodAnalyzer;
+
+
+namespace UnitTest
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        // TC 2.1
+        [Test]
+        [TestCase("I am in Sad Mood")]
+        public void Test1_AnalyzeMood_PassingValidInput_ReturnTrue(string input)
+        {
+            MoodAnalyser msg = new MoodAnalyser(input);
+            string expected = "Happy";
+
+            var actual = msg.AnalyseMood();
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        /*//TC 1.2
+        [Test]
+        [TestCase("I am in Any Mood")]
+        public void Test2_AnalyzeMood_PassingValidInput_ReturnTrue(string input)
+        {
+            MoodAnalyser msg = new MoodAnalyser(input);
+            string expected = "Happy";
+
+            var actual = msg.AnalyseMood();
+
+            Assert.AreEqual(expected, actual);
+        }*/
+    }
+}
